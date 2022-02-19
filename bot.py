28aca550cf6a460faff6b9cbc4eb0d47fd72f1da
kinuskicoder <kinuskigamer@gmail.com>
2022-02-19 18:03:36 +0200

    commit

--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='-w')
+bot = commands.Bot(command_prefix='? ')
 
 @bot.event
 async def on_ready():
-    print(f"The bot has logged in as {bot.user.name & bot.user,id}")
+    print(f"The bot has logged in as {bot.user.name, bot.user.id}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("hey")
 
 
 bot.run('token')
