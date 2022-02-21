b618d278baecfdd804483c153eab8d3a87b64eda
kinuskicoder <kinuskigamer@gmail.com>
2022-02-21 20:37:48 +0200

    committing changes

--- cogs/watcher.py
+++ cogs/watcher.py
@@ -4,12 +4,17 @@ from discord.ext import commands
 
 class watcher(commands.Cog):
     def __init__(self, bot):
-        self.client = bot
+        self.bot = bot
 
     @commands.command()
     async def repeat(self, ctx, *, rep):
         await ctx.send(rep)
 
+    @commands.Cog.listener('on_message')
+    async def msg_ev(ctx, message):
+        if message.author.bot == False:
+            str = f"{message.author} sent message: {message.content}"
+            await message.channel.send(str)
     
 def setup(bot):
     bot.add_cog(watcher(bot))
